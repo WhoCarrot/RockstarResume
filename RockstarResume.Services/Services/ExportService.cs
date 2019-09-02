75912568b3f3df94d7ff7ad34defb641c26c79b8
Maurits <mauritsvandelagemaat@hotmail.com>
2019-09-02 12:38:20 +0200

    Add demo export to docx option

---
+++ RockstarResume.Services/Services/ExportService.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Text;
+using DocumentFormat.OpenXml;
+using DocumentFormat.OpenXml.InkML;
+using DocumentFormat.OpenXml.Packaging;
+using DocumentFormat.OpenXml.Wordprocessing;
+using RockstarResume.Model;
+
+namespace RockstarResume.Logic.Services
+{
+    public class ExportService
+    {
+        /// <summary>
+        /// For demo purposes, write the rockstar firstname and lastname to a docx file and return the byte array.
+        /// </summary>
+        /// <param name="resume"></param>
+        /// <returns></returns>
+        public byte[] ExportDocx(Resume resume)
+        {
+            using (var memoryStream = new MemoryStream())
+            {
+                using (var document = WordprocessingDocument.Create(memoryStream, WordprocessingDocumentType.Document, true))
+                {
+                    var mainPart = document.AddMainDocumentPart();
+                    
+                    mainPart.Document = new Document();
+                    var documentBody = new Body();
+
+                    var paragraph = new Paragraph();
+                    var run = new Run();;
+                    var text = new Text($"Dit is een demo CV van {resume.Rockstar.FirstName} {resume.Rockstar.LastName}!");
+
+                    run.AppendChild(text);
+                    paragraph.AppendChild(run);
+                    documentBody.AppendChild(paragraph);
+                }
+
+                return memoryStream.ToArray();
+            }
+        }
+    }
+}
---
+++ RockstarResume/Controllers/ExportController.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Net.Http.Headers;
+using RockstarResume.Logic.Services;
+using RockstarResume.Services;
+
+namespace RockstarResume.App.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ExportController : ControllerBase
+    {
+        private readonly ExportService _exportService;
+        private readonly ResumeService _resumeService;
+
+        public ExportController(ExportService exportService, ResumeService resumeService)
+        {
+            _exportService = exportService;
+            _resumeService = resumeService;
+        }
+
+        public IActionResult Get(int resumeId)
+        {
+            var resume = _resumeService.Get(resumeId);
+            if (resume == null) return NotFound();
+            var documentBytes = _exportService.ExportDocx(resume);
+            return new FileContentResult(documentBytes, MediaTypeHeaderValue.Parse("docx"));
+        }
+    }
+}
